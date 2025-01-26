717a80eb1a6d281ae78232d7b6447b935b8eded0
eXory2024 <exory@anio.software>
2025-01-26 02:47:44 +0100

    Add getDefinedEntities.mjs.

---
+++ getDefinedEntities.mjs
@@ -0,0 +1,34 @@
+import {scandir} from "@aniojs/node-fs-scandir"
+import {fileURLToPath} from "node:url"
+import path from "node:path"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export async function getDefinedEntities() {
+	const map = new Map()
+	const entries = await scandir(path.join(__dirname, "src", "entities"))
+
+	for (const entry of entries) {
+		if (entry.type !== "regularFile") continue
+		if (entry.parents.length !== 2) continue
+		if (!entry.name.startsWith("rev")) continue
+
+		const entityName = entry.parents[0]
+		const entityMajorVersion = parseInt(entry.parents[1].slice(1), 10)
+		const entityRevision = parseInt(entry.name.slice(3), 10)
+
+		if (!map.has(entityName)) {
+			map.set(entityName, new Map())
+		}
+
+		const entityMap = map.get(entityName)
+
+		if (!entityMap.has(entityMajorVersion)) {
+			entityMap.set(entityMajorVersion, [])
+		}
+
+		entityMap.get(entityMajorVersion).push(entityRevision)
+	}
+
+	return map
+}
