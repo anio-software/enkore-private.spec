629309e828053707702fc2b46cabf83afc49479a
eXory2024 <exory@anio.software>
2025-04-06 23:12:08 +0200

    Automatically create "create<XXX>" factory functions for public entities.

---
+++ generateEntityFactoryCode.mjs
@@ -0,0 +1,16 @@
+export function generateEntityFactoryCode(
+	factoryName,
+	entityName,
+	importPath,
+	majorVersion,
+	revision
+) {
+	return `
+import type {Definition} from "${importPath}"
+import {createEntity} from "#~src/export/createEntity.mts"
+
+export function ${factoryName}(data: Definition) {
+	return createEntity("${entityName}", ${majorVersion}, ${revision}, data)
+}
+`.slice(1)
+}
