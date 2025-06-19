035b583586f89017833939a00f580f266cc1f20c
eXory2024 <exory@anio.software>
2025-06-19 19:10:54 +0200

    Add comment.

--- project/src/entities/EnkoreJSRuntimeEmbeddedFile/v0/rev0.ts
+++ project/src/entities/EnkoreJSRuntimeEmbeddedFile/v0/rev0.ts
@@ -1,5 +1,7 @@
 export type Definition = {
 	_projectIdentifier: string
+
+	// indicates whether this embedded file was requested as an URL
 	_createResourceAtRuntimeInit: boolean
 
 	projectId: string
