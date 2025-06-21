4953f287af8be6f256eb44b87da3b100fe1dcd6b
eXory2024 <exory@anio.software>
2025-06-21 18:35:18 +0200

    Add and use CommonTargetJSXOptions.

--- project/src/entities/EnkoreTargetJSXNoneOptions/v0/rev0.ts
+++ project/src/entities/EnkoreTargetJSXNoneOptions/v0/rev0.ts
@@ -1,3 +1,4 @@
 import type {CommonTargetJSOptions} from "#~src/CommonTargetJSOptions.ts"
+import type {CommonTargetJSXOptions} from "#~src/CommonTargetJSXOptions.ts"
 
-export type Definition =  CommonTargetJSOptions
+export type Definition =  CommonTargetJSOptions & CommonTargetJSXOptions
