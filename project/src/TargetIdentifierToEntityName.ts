e69198d932917cbe4e15471234ef8de7ea7e798f
eXory2024 <exory@anio.software>
2025-06-21 18:21:43 +0200

    Update TargetIdentifierToEntityName.

--- project/src/TargetIdentifierToEntityName.ts
+++ project/src/TargetIdentifierToEntityName.ts
@@ -3,6 +3,16 @@ import type {TargetIdentifier} from "@anio-software/enkore-private.primitives"
 export type TargetIdentifierToEntityName<
 	T extends TargetIdentifier
 > =
-    T extends "c"  ? "EnkoreTargetCOptions"  :
-    T extends "js" ? "EnkoreTargetJSOptions" :
+    T extends "c"          ? "EnkoreTargetCOptions"         :
+
+    T extends "js-none"    ? "EnkoreTargetJSNoneOptions"    :
+    T extends "js-web"     ? "EnkoreTargetJSWebOptions"     :
+    T extends "js-node"    ? "EnkoreTargetJSNodeOptions"    :
+    T extends "js-hybrid"  ? "EnkoreTargetJSHybridOptions"  :
+
+    T extends "jsx-none"   ? "EnkoreTargetJSXNoneOptions"   :
+    T extends "jsx-web"    ? "EnkoreTargetJSXWebOptions"    :
+    T extends "jsx-node"   ? "EnkoreTargetJSXNodeOptions"   :
+    T extends "jsx-hybrid" ? "EnkoreTargetJSXHybridOptions" :
+
     never
