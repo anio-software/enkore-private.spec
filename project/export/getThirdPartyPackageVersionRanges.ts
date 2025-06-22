b287d551e48507af45a02a1c9bba5b71abac2593
eXory2024 <exory@anio.software>
2025-06-22 13:02:08 +0200

    Add getThirdPartyPackageVersionRanges.

---
+++ project/export/getThirdPartyPackageVersionRanges.ts
@@ -0,0 +1,13 @@
+export function getThirdPartyPackageVersionRanges() {
+	return {
+		"@types/web": ">=0.0.235",
+		"@types/node": ">=22.7.x",
+
+		"@types/react": ">=19.1.x",
+		"@types/react-dom": ">=19.1.x",
+		"react": ">=19.1.x",
+		"react-dom": ">=19.1.x",
+
+		"css-modules-ts-plugin": ">=0.0.8"
+	}
+}
