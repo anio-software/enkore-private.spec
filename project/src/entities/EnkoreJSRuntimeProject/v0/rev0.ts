2bfee03b4ce43a6b69d635603dbd9dd8324e2f77
eXory2024 <exory@anio.software>
2025-07-05 18:55:33 +0200

    I think it should be safe to use the EnkoreConfig type here again.
    It was mostly the toolchain related properties that caused problems here.

--- project/src/entities/EnkoreJSRuntimeProject/v0/rev0.ts
+++ project/src/entities/EnkoreJSRuntimeProject/v0/rev0.ts
@@ -1,11 +1,9 @@
+import type {EnkoreConfig} from "#~export/__aggregatedExports.ts"
 import type {NodePackageJSON} from "@anio-software/enkore-private.primitives"
+import type {JSONCompatibleType} from "#~export/JSONCompatibleType.ts"
 
 export type Definition = {
 	projectId: string
 	packageJSON: NodePackageJSON
-	enkoreConfiguration: {
-		buildConstants?: Record<string, string>
-
-		[prop: string]: unknown
-	}
+	enkoreConfiguration: JSONCompatibleType<EnkoreConfig>
 }
