148bdf6a34fa3aa201429b0dc0088b6298e249fd
eXory2024 <exory@anio.software>
2025-07-06 22:33:52 +0200

    Update TargetIdentifierToEntityName.

--- project/src/TargetIdentifierToEntityName.ts
+++ project/src/TargetIdentifierToEntityName.ts
@@ -3,16 +3,17 @@ import type {TargetIdentifier} from "@anio-software/enkore-private.primitives"
 export type TargetIdentifierToEntityName<
 	T extends TargetIdentifier
 > =
-    T extends "c"          ? "EnkoreTargetCOptions"         :
+    T extends "c"              ? "EnkoreTargetCOptions"            :
 
-    T extends "js-none"    ? "EnkoreTargetJSNoneOptions"    :
-    T extends "js-web"     ? "EnkoreTargetJSWebOptions"     :
-    T extends "js-node"    ? "EnkoreTargetJSNodeOptions"    :
-    T extends "js-hybrid"  ? "EnkoreTargetJSHybridOptions"  :
+    T extends "js-none"        ? "EnkoreTargetJSNoneOptions"       :
+    T extends "js-web"         ? "EnkoreTargetJSWebOptions"        :
+    T extends "js-node"        ? "EnkoreTargetJSNodeOptions"       :
+    T extends "js-hybrid"      ? "EnkoreTargetJSHybridOptions"     :
+    T extends "js-hybrid-lite" ? "EnkoreTargetJSHybridLiteOptions" :
 
-//  T extends "jsx-none"   ? "EnkoreTargetJSXNoneOptions"   :
-    T extends "jsx-web"    ? "EnkoreTargetJSXWebOptions"    :
-//  T extends "jsx-node"   ? "EnkoreTargetJSXNodeOptions"   :
-    T extends "jsx-hybrid" ? "EnkoreTargetJSXHybridOptions" :
+//  T extends "jsx-none"       ? "EnkoreTargetJSXNoneOptions"      :
+    T extends "jsx-web"        ? "EnkoreTargetJSXWebOptions"       :
+//  T extends "jsx-node"       ? "EnkoreTargetJSXNodeOptions"      :
+    T extends "jsx-hybrid"     ? "EnkoreTargetJSXHybridOptions"    :
 
     never
