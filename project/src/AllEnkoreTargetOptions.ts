8725a9ed7286f6921a3f6dc8fe726492e9f6d6f8
eXory2024 <exory@anio.software>
2025-07-06 22:32:59 +0200

    Update AllEnkoreTargetOptions.

--- project/src/AllEnkoreTargetOptions.ts
+++ project/src/AllEnkoreTargetOptions.ts
@@ -4,18 +4,20 @@ import type {
 	EnkoreTargetJSWebOptions,
 	EnkoreTargetJSNodeOptions,
 	EnkoreTargetJSHybridOptions,
+	EnkoreTargetJSHybridLiteOptions,
 //	EnkoreTargetJSXNoneOptions,
 	EnkoreTargetJSXWebOptions,
 //	EnkoreTargetJSXNodeOptions,
 	EnkoreTargetJSXHybridOptions
 } from "#~export/__aggregatedExports.ts"
 
-export type AllEnkoreTargetOptions = EnkoreTargetCOptions         |
-                                     EnkoreTargetJSNoneOptions    |
-                                     EnkoreTargetJSWebOptions     |
-                                     EnkoreTargetJSNodeOptions    |
-                                     EnkoreTargetJSHybridOptions  |
-//                                   EnkoreTargetJSXNoneOptions   |
-                                     EnkoreTargetJSXWebOptions    |
-//                                   EnkoreTargetJSXNodeOptions   |
+export type AllEnkoreTargetOptions = EnkoreTargetCOptions            |
+                                     EnkoreTargetJSNoneOptions       |
+                                     EnkoreTargetJSWebOptions        |
+                                     EnkoreTargetJSNodeOptions       |
+                                     EnkoreTargetJSHybridOptions     |
+                                     EnkoreTargetJSHybridLiteOptions |
+//                                   EnkoreTargetJSXNoneOptions      |
+                                     EnkoreTargetJSXWebOptions       |
+//                                   EnkoreTargetJSXNodeOptions      |
                                      EnkoreTargetJSXHybridOptions
