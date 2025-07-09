e61ac867b1f43ecf0ccb45265e1bbfdbc0df308f
eXory2024 <exory@anio.software>
2025-07-10 00:56:53 +0200

    Make embeds an object.

--- project/src/entities/EnkoreBuildManifestFile/v0/rev0.ts
+++ project/src/entities/EnkoreBuildManifestFile/v0/rev0.ts
@@ -1,5 +1,12 @@
+type Embed = {
+	identifier: string
+
+	// indicates whether this embedded file was requested as an URL
+	createResourceAtRuntimeInit: boolean
+}
+
 export type Definition = {
 	exports: Record<string, {
-		embeds: string[]
+		embeds: Embed[]
 	}>
 }
