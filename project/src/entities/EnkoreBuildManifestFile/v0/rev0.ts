9c6a321bbbd7882978b60d957b470f23d5795922
eXory2024 <exory@anio.software>
2025-07-10 01:02:19 +0200

    Add integrity property.

--- project/src/entities/EnkoreBuildManifestFile/v0/rev0.ts
+++ project/src/entities/EnkoreBuildManifestFile/v0/rev0.ts
@@ -3,6 +3,9 @@ type Embed = {
 
 	// indicates whether this embedded file was requested as an URL
 	createResourceAtRuntimeInit: boolean
+
+	// sha256 hash
+	integrity: string
 }
 
 export type Definition = {
