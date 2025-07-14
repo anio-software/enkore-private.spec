b6c11a130f1dcf4330dcaa46a70e3958d0b2128b
eXory2024 <exory@anio.software>
2025-07-15 01:02:42 +0200

    I know this is a bit wasteful, but just create the resource URLs for all embeds at startup (for node JS project API).

--- project/src/entities/EnkoreJSRuntimeProjectAPIContext/v0/rev0.ts
+++ project/src/entities/EnkoreJSRuntimeProjectAPIContext/v0/rev0.ts
@@ -1,10 +1,13 @@
-import type {
-	EnkoreJSRuntimeProject,
-	EnkoreJSRuntimeEmbeddedFile
-} from "#~src/AllDefinedEntities.ts"
+import type {EnkoreJSRuntimeProject} from "#~src/AllDefinedEntities.ts"
+import type {DistributiveOmit} from "#~src/DistributiveOmit.ts"
+import type {Definition as EmbeddedFile} from "#~src/entities/EnkoreJSRuntimeEmbeddedFile/v0/rev0.ts"
+
+type Embed = DistributiveOmit<EmbeddedFile, "createResourceAtRuntimeInit"> & {
+	_resourceURL: string
+}
 
 export type Definition = {
 	project: EnkoreJSRuntimeProject
 
-	_projectEmbedFileMapRemoveMeInBundle?: Map<string, EnkoreJSRuntimeEmbeddedFile>
+	_projectEmbedFileMapRemoveMeInBundle?: Map<string, Embed>
 }
